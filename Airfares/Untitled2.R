810d586d1dad4fb1d621050335bf75b3dcb8c294
adnaanchida <64286274+adnaanchida@users.noreply.github.com>
2023-10-20 12:47:50 -0500

    Add files via upload

---
+++ Airfares/Untitled2.R
@@ -0,0 +1,50 @@
+# Load the necessary libraries
+install.packages("readr")
+install.packages("dplyr")
+library(readr)
+library(dplyr)
+
+# Read the data from the CSV file
+data <- read_csv("airfares2_r.csv")
+
+# Renaming the columns for clarity
+colnames(data) <- c("Flight", "Service", "Cost")
+
+# One-way ANOVA
+anova_result <- aov(Cost ~ Service, data = data)
+
+# Summarize the ANOVA results
+summary(anova_result)
+
+# Extract the ANOVA table
+anova_table <- anova(anova_result)
+
+# Extract the mean squares
+mean_sq_treatment <- anova_table[["Mean Sq"]][1]
+mean_sq_error <- anova_table[["Mean Sq"]][2]
+
+# Calculate the degrees of freedom
+df_treatment <- anova_table[["Df"]][1]
+df_error <- anova_table[["Df"]][2]
+
+# Calculate the F-statistic
+f_statistic <- mean_sq_treatment / mean_sq_error
+
+# Calculate the p-value
+p_value <- pf(f_statistic, df1 = df_treatment, df2 = df_error, lower.tail = FALSE)
+
+# Output the requested values
+cat("Sum of squares, treatment =", sum(anova_table[["Sum Sq"]])[1], "\n")
+cat("Sum of squares, error =", sum(anova_table[["Sum Sq"]])[2], "\n")
+cat("Mean squares, treatment =", mean_sq_treatment, "\n")
+cat("Mean squares, error =", mean_sq_error, "\n")
+cat("The value of the test statistics =", f_statistic, "\n")
+cat("The p-value =", p_value, "\n")
+
+# Determine whether to accept or reject the null hypothesis
+alpha <- 0.05
+if (p_value < alpha) {
+  cat("The null hypothesis is rejected. There are significant differences in the mean price of one-way airline tickets for the three travel agency websites.")
+} else {
+  cat("The null hypothesis is accepted. It means that there are no significant differences in the mean price of one-way airline tickets for the three travel agency websites.")
+}
